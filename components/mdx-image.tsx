78c1e47430ad0956f821b901dd45f849a841bfac
Satrio Wicaksono <satwica69@gmail.com>
2026-01-11 10:47:31 +0700

    feat: rework image zoom using dialog component

--- components/mdx-image.tsx
+++ components/mdx-image.tsx
@@ -3,8 +3,15 @@
 import type { ImageProps } from "next/image";
 
 import Image from "next/image";
-import * as React from "react";
 
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 
 interface MDXImageProps extends ImageProps {
@@ -13,24 +20,53 @@ interface MDXImageProps extends ImageProps {
 }
 
 export function MDXImage({ caption, alt, ...props }: MDXImageProps) {
-  const [isImageLoading, setImageLoading] = React.useState(true);
-
   return (
     <div className="not-prose my-10 flex w-full flex-col items-center gap-4">
-      <Image
-        unoptimized
-        loading="lazy"
-        alt={alt}
-        width={1000}
-        height={1000}
-        sizes="100vw"
-        className={cn(
-          "bg-muted h-auto w-full rounded-xl border object-cover object-center",
-          isImageLoading ? "blur-sm" : "blur-none"
-        )}
-        onLoad={() => setImageLoading(false)}
-        {...props}
-      />
+      <Dialog>
+        <DialogTrigger asChild>
+          <button
+            type="button"
+            aria-label="Open image in full screen"
+            className="relative w-full cursor-zoom-in! rounded-xl text-left"
+          >
+            <Image
+              unoptimized
+              alt={alt}
+              width={1000}
+              height={1000}
+              sizes="100vw"
+              className={cn(
+                "bg-muted h-auto w-full rounded-xl border object-contain"
+              )}
+              {...props}
+            />
+          </button>
+        </DialogTrigger>
+
+        <DialogContent className="h-screen w-screen border-none bg-transparent p-0 shadow-none sm:max-w-none">
+          <DialogTitle className="sr-only">{alt}</DialogTitle>
+
+          <DialogDescription className="sr-only" />
+
+          <DialogClose asChild>
+            <button
+              type="button"
+              aria-label="Close image preview"
+              className="relative flex h-screen w-screen cursor-zoom-out! items-center justify-center"
+            >
+              <div className="relative h-full w-full">
+                <Image
+                  unoptimized
+                  fill
+                  alt={alt}
+                  className="object-contain"
+                  {...props}
+                />
+              </div>
+            </button>
+          </DialogClose>
+        </DialogContent>
+      </Dialog>
 
       {caption && (
         <sub className="text-muted-foreground text-sm font-medium">
