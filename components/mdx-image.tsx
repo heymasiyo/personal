87f74838c92119a8279cf377e5e5e7b8a9442299
Satrio Wicaksono <satwica69@gmail.com>
2026-01-11 09:37:14 +0700

    refactor: remove image zoom feature and uninstall react-image-zoom

--- components/mdx-image.tsx
+++ components/mdx-image.tsx
@@ -5,7 +5,6 @@ import type { ImageProps } from "next/image";
 import Image from "next/image";
 import * as React from "react";
 
-import { ImageZoom } from "@/components/image-zoom";
 import { cn } from "@/lib/utils";
 
 interface MDXImageProps extends ImageProps {
@@ -18,22 +17,20 @@ export function MDXImage({ caption, alt, ...props }: MDXImageProps) {
 
   return (
     <div className="not-prose my-10 flex w-full flex-col items-center gap-4">
-      <ImageZoom>
-        <Image
-          unoptimized
-          loading="lazy"
-          alt={alt}
-          width={1000}
-          height={1000}
-          sizes="100vw"
-          className={cn(
-            "bg-muted h-auto w-full rounded-xl border object-cover object-center",
-            isImageLoading ? "blur-sm" : "blur-none"
-          )}
-          onLoad={() => setImageLoading(false)}
-          {...props}
-        />
-      </ImageZoom>
+      <Image
+        unoptimized
+        loading="lazy"
+        alt={alt}
+        width={1000}
+        height={1000}
+        sizes="100vw"
+        className={cn(
+          "bg-muted h-auto w-full rounded-xl border object-cover object-center",
+          isImageLoading ? "blur-sm" : "blur-none"
+        )}
+        onLoad={() => setImageLoading(false)}
+        {...props}
+      />
 
       {caption && (
         <sub className="text-muted-foreground text-sm font-medium">
